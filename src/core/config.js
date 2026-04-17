ceb2131ba25221f49fb229815f0d098eab29ba5d
Maxim_Kyianytsia <MaksKi.Wish@gmail.com>
2026-04-17 12:47:38 +0300

    fix: add config.js to git tracking and remove from gitignore

---
+++ src/core/config.js
@@ -0,0 +1,5 @@
+export const APP_CONFIG = {
+  SUPABASE_URL: 'https://pkdnyonrejptotlpzclq.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrZG55b25yZWpwdG90bHB6Y2xxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3MDI1MDksImV4cCI6MjA5MDI3ODUwOX0.4wffStFMBVmHiWQrrB9qu2ptGIu9ok3CWV79sp0M8s8',
+  GOOGLE_CLIENT_ID: '1002964432188-0o8a72qnpbjvksd9h4qgqr4r3cnb84es.apps.googleusercontent.com'
+};
